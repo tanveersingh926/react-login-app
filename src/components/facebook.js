a8c1ed3cdfb0a4f37b9be9f704260bb3321ba5b2
tanveersingh926 <tanveer.singh926@gmail.com>
2018-05-30 19:56:15 +0800

    Displayed authenticated user name, email, picture

--- src/components/facebook.js
+++ src/components/facebook.js
@@ -6,11 +6,18 @@ export default class Facebook extends Component {
          isLoggedIn:false
         ,userId:""
         ,name:""
-        ,emai:""
+        ,email:""
         ,picture:""
     }
 
     responseFacebook =(response) => {
+        this.setState({
+            isLoggedIn:true
+            ,userId:response.userID
+            ,name:response.name
+            ,email:response.email
+            ,picture:response.picture.data.url
+        })
         console.log(response);
     }
     componentClicked =() => {
@@ -18,10 +25,21 @@ export default class Facebook extends Component {
     }
 
     render() {
-        let fbContent;
+        let fbContent = (
+            <div style={{
+                width: "400px",
+                margin: "auto",
+                background: "#f4f4f4",
+                padding: "20px"
+            }}> 
+            <img src={this.state.picture} alt={this.state.name} />
+            <h2>Welcome {this.state.name}</h2>
+            Email: {this.state.email}
+            
+            </div>);
 
         if(this.state.isLoggedIn){
-            fbContent=null
+            fbContent=fbContent
         } else {
             fbContent = <FacebookLogin
             appId="2106343566278696"
